990467d4ae77d7e927343d1a46291e8f1f9f1283
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-03-24 02:12:43 +0700

    update câu 2

--- Cau2_AppLenghthConverter/app/src/main/java/ntu/pl63132227/cau2_applenghthconverter/MainActivity.java
+++ Cau2_AppLenghthConverter/app/src/main/java/ntu/pl63132227/cau2_applenghthconverter/MainActivity.java
@@ -15,12 +15,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nhapso;
+    RadioButton rdkilomet, rdhectomet, rddecamet, rddmet, rddecimet, rdcentimet, rdmilimet;
+    RadioButton rskilomet, rshectomet, rsdecamet, rsdmet, rsdecimet, rscentimet, rsmilimet;
+    Button buttondoi;
+    TextView ketqua, thongbao;
+    void layDL (){
+        nhapso = findViewById(R.id.txtnhap);
+        rdkilomet = findViewById(R.id.rdKm);
+        rdhectomet = findViewById(R.id.rdHm);
+        rddecamet = findViewById(R.id.rdDam);
+        rddmet = findViewById(R.id.rdM);
+        rddecimet = findViewById(R.id.rdDm);
+        rdcentimet = findViewById(R.id.rdCm);
+        rdmilimet = findViewById(R.id.rdMm);
 
+        rskilomet = findViewById(R.id.rsKm);
+        rshectomet = findViewById(R.id.rsHm);
+        rsdecamet = findViewById(R.id.rsDam);
+        rsdmet = findViewById(R.id.rsM);
+        rsdecimet = findViewById(R.id.rsDm);
+        rscentimet = findViewById(R.id.rsCm);
+        rsmilimet = findViewById(R.id.rsMm);
+
+        buttondoi = findViewById(R.id.btndoi);
+        ketqua = findViewById(R.id.txtkq);
+        thongbao = findViewById(R.id.txtthongbao);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layDL();
+        buttondoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if(!XuLy()){
+                return;
+            }
 
+
+            }
+        });
     }
-
+    private boolean XuLy(){
+        
+    }
 }
