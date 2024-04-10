5fbae6c85c565dd36ffe2b5c7fa61581fef752ba
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-04-11 03:13:12 +0700

    Hoàn thành recyclerview

--- RecyclerView/app/src/main/java/ntu/dpl63132227/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/ntu/dpl63132227/recyclerview/MainActivity.java
@@ -7,18 +7,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewData;
+    RecyclerView recyclerViewLandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //3
+        recyclerViewData = getRecyclerViewData();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewData);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getRecyclerViewData(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<>();
+        //LandScape landScape1 = new LandScape("flag_tower_of_hanoi", "Cột cờ Hà Nội");
+        //dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("flag_tower_of_hanoi","Cột cờ Hà Nội"));
+        dsDuLieu.add(new LandScape("eiffel","Tháp Eiffel"));
+        dsDuLieu.add(new LandScape("buckingham","Cung điện Buckingham"));
+        dsDuLieu.add(new LandScape("statue_of_liberty","Tượng nữ thần tự do"));
+        return dsDuLieu;
     }
 }
