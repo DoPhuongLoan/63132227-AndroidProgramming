fe28fe75b54bc9d7b633d44a4195b2cb7b0ce474
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-04-22 23:47:43 +0700

    update VD1_ExplicitIntents

--- Bai1_ExplicitIntents/app/src/main/java/ntu/dpl63132227/bai1_explicitintents/MainActivity.java
+++ Bai1_ExplicitIntents/app/src/main/java/ntu/dpl63132227/bai1_explicitintents/MainActivity.java
@@ -1,24 +1,21 @@
 package ntu.dpl63132227.bai1_explicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void ChuyenManHinh(View v) {
+        Intent ManHinh2 = new Intent(this, SubOneActivity.class);
+        startActivity(ManHinh2);
     }
 }
--- Bai1_ExplicitIntents/app/src/main/java/ntu/dpl63132227/bai1_explicitintents/SubOneActivity.java
+++ Bai1_ExplicitIntents/app/src/main/java/ntu/dpl63132227/bai1_explicitintents/SubOneActivity.java
@@ -1,4 +1,21 @@
 package ntu.dpl63132227.bai1_explicitintents;
 
-public class SubOneActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub_one);
+    }
+
+    public void QuayVe(View v) {
+        Intent ManHinhChinh = new Intent(this, MainActivity.class);
+        startActivity(ManHinhChinh);
+    }
 }
