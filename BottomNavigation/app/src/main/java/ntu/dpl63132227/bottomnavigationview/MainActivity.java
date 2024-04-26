62292706833a8b5fc13e55887021d71664c74868
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-04-26 14:14:01 +0700

    Update BottomNavigation

--- BottomNavigation/app/src/main/java/ntu/dpl63132227/bottomnavigationview/MainActivity.java
+++ BottomNavigation/app/src/main/java/ntu/dpl63132227/bottomnavigationview/MainActivity.java
@@ -7,18 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main, new Home_nav()).commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.home) {
+            selectedFragment = new Home_nav();
+        } else if (itemId == R.id.music) {
+            selectedFragment = new Music_nav();
+        } else if (itemId == R.id.notify) {
+            selectedFragment = new Notification_nav();
+        }
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main, selectedFragment).commit();
+        }
+        return true;
+    };
 }
